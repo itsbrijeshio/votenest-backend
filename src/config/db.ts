b9cb90561125308e3f3bebbed9e749757badb0ce
itsbrijeshio <bk6500416@gmail.com>
2025-08-05 09:17:47 +0530

    Debug env in production

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI", process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI as string);
     console.info("DB Connected successfully!");
   } catch (error) {
