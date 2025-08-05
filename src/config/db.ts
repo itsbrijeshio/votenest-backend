f5679dd00ab271974d42192d40084cb4cde84a36
itsbrijeshio <bk6500416@gmail.com>
2025-08-05 09:30:41 +0530

    remove env from db log

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,6 @@ import env from "./env";
 
 const connectDB = async () => {
   try {
-    console.log("MONGO_URI", env.MONGO_URI);
     await mongoose.connect(env.MONGO_URI);
     console.info("DB Connected successfully!");
   } catch (error) {
